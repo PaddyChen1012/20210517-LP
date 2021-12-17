349ad20fd5ba598a38c5adab39e9a37f1f131340
DESKTOP-K767DRA\Paddy <paddy_chen@atarroba.com>
2021-12-17 12:00:39 +0800

    只要點擊就下一頁

--- 2021119_自動跳轉/index.php
+++ 2021119_自動跳轉/index.php
@@ -35,14 +35,7 @@ $country =  $_SERVER['HTTP_CF_IPCOUNTRY'];
                 <a href="https://play.google.com/store/apps/details?id=hello.tr.slot" class="position-absolute d-flex align-items-center justify-content-center font-weight-bold try-btn"><span class="h1 m-0">PLAY NOW</span></a>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="display: none;">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"></a>
     </div>
     <script>
         var country ='<?=  $country  ?>';
